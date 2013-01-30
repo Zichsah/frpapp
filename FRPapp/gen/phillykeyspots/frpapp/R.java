e795201720f31b59e558d04d3a36c6198bfe30be
btopportaldev <btoppotaldev@coe.drexel.edu>
2013-01-30 15:59:11 -0500

    update all files

--- FRPapp/gen/phillykeyspots/frpapp/R.java
+++ FRPapp/gen/phillykeyspots/frpapp/R.java
@@ -14,18 +14,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int b_events=0x7f070001;
+        public static final int b_finder=0x7f070000;
+        public static final int b_joml=0x7f070003;
+        public static final int b_resources=0x7f070002;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_find_events=0x7f030000;
+        public static final int activity_find_resources=0x7f030001;
+        public static final int activity_join_our_mailing_list=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_search_for_keyspot=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_find_events=0x7f060000;
+        public static final int activity_find_resources=0x7f060001;
+        public static final int activity_join_our_mailing_list=0x7f060002;
+        public static final int activity_main=0x7f060003;
+        public static final int activity_search_for_keyspot=0x7f060004;
+        public static final int activity_search_for_training_events=0x7f060005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int button_events=0x7f040002;
+        public static final int button_finder=0x7f040001;
+        public static final int button_joml=0x7f040004;
+        public static final int button_resources=0x7f040003;
+        public static final int hello_world=0x7f040006;
+        public static final int menu_settings=0x7f040005;
+        public static final int title_activity_find_events=0x7f04000a;
+        public static final int title_activity_find_resources=0x7f040008;
+        public static final int title_activity_join_our_mailing_list=0x7f040009;
+        public static final int title_activity_search_for_keyspot=0x7f040007;
     }
     public static final class style {
         /** 
