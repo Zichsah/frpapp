3c5bc41f5b119981699b9308e5315dd8a2e27554
btopportaldev <btoppotaldev@coe.drexel.edu>
2013-01-31 14:40:28 -0500

    Final commit of 1/31 for noah

--- FRPapp/src/phillykeyspots/frpapp/MainActivity.java
+++ FRPapp/src/phillykeyspots/frpapp/MainActivity.java
@@ -2,6 +2,7 @@ package phillykeyspots.frpapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -10,6 +11,10 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void dashboard(View view){
+		
 	}
 
 }
