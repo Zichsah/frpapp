56adf7cf205f9ffe3510b18cabe160dcaa1357b9
Maria Yala <btopportal@coe.drexel.edu>
2013-03-08 08:53:45 -0500

    Added ContactUs and AboutUs page

--- FRPapp/src/phillykeyspots/frpapp/MainActivity.java
+++ FRPapp/src/phillykeyspots/frpapp/MainActivity.java
@@ -3,8 +3,10 @@ package phillykeyspots.frpapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-
 
 public class MainActivity extends FragmentActivity {
 
@@ -13,6 +15,30 @@ public class MainActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+	
+	 // Handles the user's menu selection.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_aboutus:
+        	Intent aboutusActivity = new Intent(getBaseContext(), AboutUsActivity.class);
+            startActivity(aboutusActivity);
+            return true;
+        case R.id.menu_contactus:
+        	Intent contactusActivity = new Intent(getBaseContext(), ContactUsActivity.class);
+        	startActivity(contactusActivity);
+        	return true;
+        default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 	
 	public void dashboard(View view){
 		int id = view.getId();
